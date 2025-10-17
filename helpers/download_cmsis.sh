e8241599a0b53f3e331a2c8a65637539e8799540
Pavel Demin <pavel-demin@outlook.com>
2025-10-17 10:28:07 +0200

    update CMSIS

--- helpers/download_cmsis.sh
+++ helpers/download_cmsis.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 STM32G0_URL=https://raw.githubusercontent.com/STMicroelectronics/STM32CubeG0/master
+CMSISG0_URL=https://raw.githubusercontent.com/STMicroelectronics/cmsis-device-g0/master
 
 mkdir CMSIS
 
@@ -10,14 +11,24 @@ HEADERS="\
   Include/cmsis_version.h \
   Include/core_cm0plus.h \
   Include/mpu_armv7.h \
-  Device/ST/STM32G0xx/Include/stm32g071xx.h \
-  Device/ST/STM32G0xx/Include/stm32g0xx.h \
-  Device/ST/STM32G0xx/Include/system_stm32g0xx.h \
 "
 
 for file in $HEADERS
 do
   name=`basename $file`
   curl -L $STM32G0_URL/Drivers/CMSIS/$file -o CMSIS/$name
+  sed -i 's/\ *$//' CMSIS/$name
+done
+
+HEADERS="\
+  Include/stm32g071xx.h \
+  Include/stm32g0xx.h \
+  Include/system_stm32g0xx.h \
+"
+
+for file in $HEADERS
+do
+  name=`basename $file`
+  curl -L $CMSISG0_URL/$file -o CMSIS/$name
   sed -i 's/\ *$//' CMSIS/$name
 done
